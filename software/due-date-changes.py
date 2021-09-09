ea68b4fd09d666686b91f5952084a9cc6f2979c6
Mark Coatsworth <coatsworth@cs.wisc.edu>
2021-09-09 11:23:06 -0500

    Removed some unused bloat code (HTCONDOR-611)

--- software/due-date-changes.py
+++ software/due-date-changes.py
@@ -10,17 +10,6 @@ from datetime import datetime
 from jira import JIRA
 
 
-def dump(obj):
-    for attr in dir(obj):
-        print("obj.%s = %r" % (attr, getattr(obj, attr)))
-
-
-def print_help(stream=sys.stderr):
-    help_msg = """Usage: {0} [--projects] LIST,OF,PROJECTS
-"""
-    stream.write(help_msg.format(sys.argv[0]))
-
-
 def parse_args():
 
     parser = argparse.ArgumentParser()
@@ -40,12 +29,7 @@ def parse_args():
 
 def main():
 
-    # Parse arguments. Assume any error handling happens in parse_args()
-    try:
-        args = parse_args()
-    except Exception as err:
-        print(f"Failed to parse arguments: {err}", file=sys.stderr)
-
+    args = parse_args()
     projects = args["projects"]
     output_file = args["output_file"]
 
@@ -59,7 +43,6 @@ def main():
     # Now iterate over the projects and list their issues
     for project in projects.split(","):
         issues = jira.search_issues(f"project = {project} AND statusCategory != \"To Do\"", expand="changelog", maxResults=False)
-        #issues = jira.search_issues(f"key = HTCONDOR-344", expand="changelog", maxResults=False)
         for issue in issues:
             duedate_current = issue.fields.duedate
             duedate_original = issue.fields.duedate
